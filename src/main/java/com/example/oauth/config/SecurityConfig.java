103d924f4651f2a4c65a4de2620cd1eec69d5553
thiago.correa@ewave.com.br <thiagofarbo@gmail.com>
2021-05-04 17:58:48 -0300

    Added Dockerfile configuration

--- src/main/java/com/example/oauth/config/SecurityConfig.java
+++ src/main/java/com/example/oauth/config/SecurityConfig.java
@@ -6,10 +6,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import java.security.SecureRandom;
 
 @EnableWebSecurity
 @EnableAuthorizationServer
